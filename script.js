21e060c108094f0898968d2476beba8c5e3ae0fa
Mael <laem@kont.me>
2023-12-15 14:23:51 +0100

    Init

---
+++ script.js
@@ -0,0 +1,20 @@
+import { openDb, getStops, closeDb, getStoptimes } from "gtfs";
+import { importGtfs } from "gtfs";
+import { readFile } from "fs/promises";
+
+const config = JSON.parse(
+  await readFile(new URL("./config.json", import.meta.url))
+);
+
+try {
+  //await importGtfs(config);
+  const db = openDb(config);
+  const stops = getStoptimes({
+    stop_id: ["STAR:1320"],
+  });
+  console.log(stops);
+
+  //  closeDb(db);
+} catch (error) {
+  console.error(error);
+}
