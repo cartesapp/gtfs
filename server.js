e8a2eab83e7de98cd652597637d8ee65996e4bc7
Mael <laem@kont.me>
2023-12-15 14:52:40 +0100

    Premier service de GTFS

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 3000;
+import express from 'express'
+import { openDb, getStops, closeDb, getStoptimes } from 'gtfs'
+import { importGtfs } from 'gtfs'
+import { readFile } from 'fs/promises'
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const config = JSON.parse(
+  await readFile(new URL('./config.json', import.meta.url))
+)
+import fs from 'fs'
+const app = express()
+const port = process.env.PORT || 3000
+import { Readable } from 'node:stream'
+
+const url = 'https://www.korrigo.bzh/ftp/OPENDATA/KORRIGOBRET.gtfs.zip'
+
+const fetchGTFS = async () => {
+  const response = await fetch(url)
+  const fileWriteStream = fs.createWriteStream('./gtfs/bretagne.zip')
+  const readableStream = Readable.fromWeb(response.body)
+  readableStream.pipe(fileWriteStream)
+  setTimeout(() => {
+    importGtfs(config)
+  }, 20000)
+}
+
+fetchGTFS()
+
+app.get('/', (req, res) => {
+  try {
+    const db = openDb(config)
+    const stops = getStoptimes({
+      stop_id: ['STAR:1320'],
+    })
+    console.log(stops)
+    res.json(stops)
+
+    //  closeDb(db);
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
