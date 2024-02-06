36e9cef300ecb186648b221ac94d4c795fa0a59b
Oliv4945 <6248122+Oliv4945@users.noreply.github.com>
2024-02-06 23:30:13 +0100

    Add AURA region, let node-gtfs handle downloads

--- server.js
+++ server.js
@@ -28,14 +28,8 @@ app.use(
 const port = process.env.PORT || 3000
 import { Readable } from 'node:stream'
 
-const url = 'https://www.korrigo.bzh/ftp/OPENDATA/KORRIGOBRET.gtfs.zip'
-
 const fetchGTFS = async () => {
   console.log('will fetch gtfs zip and import in node-gtfs')
-  const response = await fetch(url)
-  const fileWriteStream = fs.createWriteStream('./gtfs/bretagne.zip')
-  const readableStream = Readable.fromWeb(response.body)
-  await pipeline(readableStream, fileWriteStream)
   await importGtfs(config)
   return "C'est bon !"
 }
