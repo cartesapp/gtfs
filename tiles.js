10497e43a7b335df9782bfef71f2ab6e1a10e1fa
Mael <laem@kont.me>
2024-09-25 21:15:28 +0200

    Logger correct

--- tiles.js
+++ tiles.js
@@ -1,6 +1,13 @@
-import { exec as rawExec } from 'child_process'
+import { exec as rawExec, spawn } from 'child_process'
 import util from 'util'
 const realExec = util.promisify(rawExec)
+import axios from 'axios'
+import debounce from 'debounce'
+import fs from 'fs'
+import path from 'path'
+
+import { MultiProgressBars } from 'multi-progress-bars'
+import * as chalk from 'chalk'
 
 const grid = ['N50E000', 'N40E010', 'N40E000', 'N50E010']
 
@@ -9,7 +16,38 @@ const dryExec = async (text) => {
   return Promise.resolve({ stdout: '', stderr: '' })
 }
 
-const exec = realExec
+const log = (...messages) => console.log(...messages)
+const liveExec = async (command) => {
+  const [program, ...args] = command.split(' ')
+
+  const promise = new Promise((resolve, reject) => {
+    const child = spawn(program, args)
+
+    child.stdout.on('data', function (data) {
+      log('🟢stdout: ' + data.toString())
+    })
+    child.stderr.on('data', function (data) {
+      const message = '🔴stderr: ' + data.toString()
+      log(message)
+      //reject(message)
+    })
+    child.on('close', function (code) {
+      const message = 'child process exited with code ' + code
+      console.log(message)
+      resolve(message)
+    })
+  })
+
+  return promise
+}
+
+/*
+liveExec(
+  'wget https://download.geofabrik.de/europe/france/bretagne-latest.osm.pbf'
+)
+*/
+
+const exec = liveExec
 
 export async function updatePlanetTiles() {
   console.log('Will download panoramax planet pmtiles')
@@ -23,15 +61,18 @@ export async function updatePlanetTiles() {
   console.log('stderr:', stderr)
 }
 
+updateFranceTiles()
 export async function updateFranceTiles() {
   // Now france tiles
 
   const logs = await Promise.all(
-    grid.map((zone) =>
-      exec(
-        `wget https://osm.download.movisda.io/grid/${zone}-10-latest.osm.pbf`
+    grid
+      .slice(0, 2)
+      .map((zone) =>
+        download(
+          `https://osm.download.movisda.io/grid/${zone}-10-latest.osm.pbf`
+        )
       )
-    )
   )
 
   const tilemakerMerges = grid.map(
@@ -55,3 +96,49 @@ export async function updateFranceTiles() {
 }
 
 //updateTiles()
+
+// Initialize mpb
+const mpb = new MultiProgressBars({
+  initMessage: ' $ Example Fullstack Build ',
+  anchor: 'top',
+  persist: true,
+  border: true,
+})
+
+function download(url) {
+  return new Promise(async (resolve, reject) => {
+    const filename = url.split('/').slice(-1)[0]
+    console.log('Will write', filename)
+    console.log('Connecting …')
+    const { data, headers } = await axios({
+      url,
+      method: 'GET',
+      responseType: 'stream',
+    })
+    const totalLength = headers['content-length']
+
+    console.log('Starting download')
+    const task = 'Downloading ' + filename
+    mpb.addTask(task, {
+      type: 'percentage',
+      barColorFn: chalk.yellow,
+    })
+    const total = parseInt(totalLength)
+
+    const writer = fs.createWriteStream(path.resolve('./', filename))
+
+    data.on('data', (chunk) =>
+      mpb.incrementTask(task, { percentage: (chunk.length / total) * 1 })
+    )
+
+    data.pipe(writer)
+
+    data.on('end', () => resolve())
+  })
+}
+
+/*
+download(
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+)
+*/
