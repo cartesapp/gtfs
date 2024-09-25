47fa4f4eae64d1a07e8862e10555c0156712f94f
Mael <laem@kont.me>
2024-09-25 19:00:21 +0200

    util manquant

--- tiles.js
+++ tiles.js
@@ -1,4 +1,5 @@
 import { exec as rawExec } from 'child_process'
+import util from 'util'
 const realExec = util.promisify(rawExec)
 
 const grid = ['N50E000-10', 'N40E010-10', 'N40E000-10', 'N50E010-10']
